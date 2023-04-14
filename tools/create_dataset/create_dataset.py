9bff145ea9664dc9b6178f9e847859040939e233
Bakeneko <monopolecat@gmail.com>
2023-04-14 19:23:58 +0900

    tools

---
+++ tools/create_dataset/create_dataset.py
@@ -0,0 +1,144 @@
+# chapter03.py
+# -*- coding: utf-8 -*-
+import csv
+import time
+import argparse
+import itertools
+from collections import deque
+import csv_output
+from datetime import datetime
+
+import cv2
+import mediapipe as mp
+
+
+# コマンドライン引数取得
+parser = argparse.ArgumentParser()
+parser.add_argument("--pat", type=int, default=0)
+parser.add_argument("--name", type=str)
+parser.add_argument("--frames", type=int, default=150)
+args = parser.parse_args()
+
+pat_dict = {
+    0:"10000",
+    1:"01000",
+    2:"00100",
+    3:"00010",
+    4:"00001"
+}
+
+csv_save = csv_output.CsvSaveService(args.name + "_" + pat_dict[args.pat] + ".csv")
+
+
+# ランドマークの画像上の位置を算出する関数
+def calc_landmark_list(image, landmarks):
+    landmark_point = []
+    image_width, image_height = image.shape[1], image.shape[0]
+
+    for _, landmark in enumerate(landmarks.landmark):
+        landmark_x = min(int(landmark.x * image_width), image_width - 1)
+        landmark_y = min(int(landmark.y * image_height), image_height - 1)
+        landmark_point.append([landmark_x, landmark_y])
+
+    return landmark_point
+
+
+# 座標履歴を描画する関数
+def draw_point_history(image, point_history):
+    for index, point in enumerate(point_history):
+        if point[0] != 0 and point[1] != 0:
+            cv2.circle(image, (point[0], point[1]), 1 + int(index / 2),
+                       (255, 0, 0), 2)
+    return image
+
+
+# CSVファイルに座標履歴を保存する関数
+def logging_csv(gesture_id, csv_path, width, height, point_history_list):
+    with open(csv_path, 'a', newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow([gesture_id, width, height, *point_history_list])
+    return
+
+
+# カメラキャプチャ設定
+camera_no = 0
+#video_capture = cv2.VideoCapture(camera_no)
+#video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+#video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
+video_capture = cv2.VideoCapture("./sample.mp4")
+
+# MediaPipe poses初期化
+# mp_poses = mp.solutions.pose
+mp_pose = mp.solutions.pose
+
+mp_drawing = mp.solutions.drawing_utils
+
+# MediaPipe pose初期化
+pose = mp_pose.Pose(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
+)
+
+
+# 人差指の指先の座標履歴を保持するための変数
+history_length = 16
+point_history = deque(maxlen=history_length)
+
+# CSVファイル保存先
+csv_path = './point_history.csv'
+
+frame_count = 0 
+while video_capture.isOpened():
+    # カメラ画像取得
+    ret, frame = video_capture.read()
+    if ret is False:
+        break
+    frame_width, frame_height = frame.shape[1], frame.shape[0]
+
+    # 鏡映しになるよう反転
+    frame = cv2.flip(frame, 1)
+
+    # MediaPipeで扱う画像は、OpenCVのBGRの並びではなくRGBのため変換
+    rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # 画像をリードオンリーにしてposes検出処理実施
+    rgb_image.flags.writeable = False
+    pose_results = pose.process(rgb_image)
+    rgb_image.flags.writeable = True
+
+    # 有効なランドマークが検出された場合、ランドマークを描画
+    if pose_results.pose_landmarks:
+        mp_drawing.draw_landmarks(frame, pose_results.pose_landmarks,
+                                    mp_pose.POSE_CONNECTIONS)
+
+        # ランドマーク座標の計算
+        landmark_list = calc_landmark_list(rgb_image, pose_results.pose_landmarks)
+        # 人差指の指先座標を履歴に追加
+        # point_history.append(landmark_list[ID_FINGER_TIP])
+        
+        result = {}
+        data_source = {}
+        for i,landmark in enumerate(pose_results.pose_landmarks.landmark):
+            result[mp_pose.PoseLandmark(i).name] = {"x":landmark.x,"y":landmark.y}
+        data_source["pose"] = result
+        data_source["timestamp"] = datetime.now()
+        csv_save.save_data(data_source)
+
+    # ディスプレイ表示
+    frame = draw_point_history(frame, point_history)
+    cv2.imshow('chapter03', frame)
+
+    # キー入力(ESC:プログラム終了)
+    key = cv2.waitKey(1)
+    if key == 27:  # ESC
+        break
+
+    if frame_count >= args.frames:
+        break
+
+    frame_count += 1
+
+# リソースの解放
+video_capture.release()
+pose.close()
+cv2.destroyAllWindows()
---
+++ tools/create_dataset/csv_output.py
@@ -0,0 +1,37 @@
+import csv
+from datetime import datetime
+
+class CsvSaveService:
+
+  def __init__(self, filepath: str):
+    self.filepath = filepath
+    self.is_first = True
+    pass
+
+  @staticmethod
+  def convert_data(data):
+    res_data = dict()
+    for bone_name, pos_dict in data["pose"].items():
+      for pos_label, pos in pos_dict.items():
+        # print(f"{bone_name}_{pos_label}") 
+        # print(pos)
+        res_data[f"{bone_name}_{pos_label}"] = pos
+    #res_data["emotion"] = data["emotion"]
+    res_data["timestamp"] = data["timestamp"]
+    return res_data
+
+  def save_data(self, data):
+    res = self.convert_data(data)
+    # print(res.keys())
+    with open(self.filepath, "a", newline="") as f:
+      writer = csv.DictWriter(f, fieldnames=res.keys())
+      if (self.is_first):
+        writer.writeheader()
+        self.is_first = False
+      writer.writerow(res)
+    
+if __name__ == "__main__": 
+  test_data = {"pose" : {"hoge": {"x": 1.0, "y": 2.0}, "fuga": {"x": 1.0, "y": 2.0}, "san": {"x": 1.0, "y": 2.0}, "hu": {"x": 1.0, "y": 2.0}}, "emotion": 1, "timestamp": datetime.now()}
+  csv_save = CsvSaveService("hoge.csv")
+  data = csv_save.save_data(test_data)
+  print(data)
