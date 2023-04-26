64cfd29f82d1053e9c6238d769d9af90976a8bb4
touka_aoi <toukaseisei@gmail.com>
2023-04-26 15:35:41 +0900

    ディレクトリを指定できるように変更

--- tools/create_dataset/create_dataset.py
+++ tools/create_dataset/create_dataset.py
@@ -1,35 +1,20 @@
 # chapter03.py
 # -*- coding: utf-8 -*-
+
+# import
 import csv
-import time
 import argparse
-import itertools
 from collections import deque
-import csv_output
 from datetime import datetime
 import os
 
 import cv2
 import mediapipe as mp
 
+# ユーザーファイルのインポート
+import csv_output
 
-# コマンドライン引数取得
-parser = argparse.ArgumentParser()
-parser.add_argument("--pat", type=int, default=0)
-parser.add_argument("--path", type=str)
-parser.add_argument("--frames", type=int, default=150)
-args = parser.parse_args()
-
-pat_dict = {
-    0:"10000",
-    1:"01000",
-    2:"00100",
-    3:"00010",
-    4:"00001"
-}
-
-csv_save = csv_output.CsvSaveService(os.path.splitext(os.path.basename(args.path))[0] + "_" + pat_dict[args.pat] + ".csv")
-
+# 関数宣言
 
 # ランドマークの画像上の位置を算出する関数
 def calc_landmark_list(image, landmarks):
@@ -63,86 +48,105 @@ def logging_csv(gesture_id, csv_path, width, height, point_history_list):
         writer.writerow([gesture_id, width, height, *point_history_list])
     return
 
+# コマンドライン引数取得
+parser = argparse.ArgumentParser()
+parser.add_argument("--pat", type=int, default=0) # ファイル名用
+parser.add_argument("--path", type=str) # ディレクトリ動画の選択
+parser.add_argument("--frames", type=int, default=150) # 取得フレーム数(30fps, 5s)
+args = parser.parse_args()
 
-# カメラキャプチャ設定
-camera_no = 0
-#video_capture = cv2.VideoCapture(camera_no)
-#video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-#video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
-video_capture = cv2.VideoCapture(args.path)
+pat_dict = {
+    0:"10000",
+    1:"01000",
+    2:"00100",
+    3:"00010",
+    4:"00001"
+}
 
 # MediaPipe poses初期化
-# mp_poses = mp.solutions.pose
 mp_pose = mp.solutions.pose
-
 mp_drawing = mp.solutions.drawing_utils
 
-# MediaPipe pose初期化
 pose = mp_pose.Pose(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5
 )
 
+# ディレクトリのパスを保存
+video_directory = args.path 
 
-# 人差指の指先の座標履歴を保持するための変数
-history_length = 16
-point_history = deque(maxlen=history_length)
+# ディレクトリ内のファイルリストを取得
+file_list = os.listdir(video_directory)
 
-# CSVファイル保存先
-csv_path = './point_history.csv'
+# すべてのファイルをCSVに保存する
+for file in file_list:
+    # 保存CSVファイルの作成
+    csv_save = csv_output.CsvSaveService(os.path.splitext(os.path.basename(file))[0] + "_" + pat_dict[args.pat] + ".csv")
+    # 動画の読み込み
+    video_capture = cv2.VideoCapture(video_directory + "/" + file)
 
-frame_count = 0
-while video_capture.isOpened():
-    
-    if frame_count >= args.frames:
-        break
-    # カメラ画像取得
-    ret, frame = video_capture.read()
-    if ret is False:
-        break
-    frame_width, frame_height = frame.shape[1], frame.shape[0]
+    # フレームカウントの初期化
+    frame_count = 0
 
-    # 鏡映しになるよう反転
-    frame = cv2.flip(frame, 1)
+    # 人差指の指先の座標履歴を保持するための変数
+    history_length = 16
+    point_history = deque(maxlen=history_length)
 
-    # MediaPipeで扱う画像は、OpenCVのBGRの並びではなくRGBのため変換
-    rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    while video_capture.isOpened():
+        if frame_count >= args.frames:
+        # 指定フレームより超えると終了
+            break
 
-    # 画像をリードオンリーにしてposes検出処理実施
-    rgb_image.flags.writeable = False
-    pose_results = pose.process(rgb_image)
-    rgb_image.flags.writeable = True
+        # カメラ画像取得
+        ret, frame = video_capture.read()
+        if ret is False:
+            break
+        frame_width, frame_height = frame.shape[1], frame.shape[0]
 
-    # 有効なランドマークが検出された場合、ランドマークを描画
-    if pose_results.pose_landmarks:
-        mp_drawing.draw_landmarks(frame, pose_results.pose_landmarks,
-                                    mp_pose.POSE_CONNECTIONS)
+        # 鏡映しになるよう反転
+        frame = cv2.flip(frame, 1)
 
-        # ランドマーク座標の計算
-        landmark_list = calc_landmark_list(rgb_image, pose_results.pose_landmarks)
-        # 人差指の指先座標を履歴に追加
-        # point_history.append(landmark_list[ID_FINGER_TIP])
+        # MediaPipeで扱う画像は、OpenCVのBGRの並びではなくRGBのため変換
+        rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        result = {}
-        data_source = {}
-        #for i,landmark in enumerate(pose_results.pose_landmarks.landmark):
-        for i,landmark in enumerate(landmark_list):
-            result[mp_pose.PoseLandmark(i).name] = {"x":landmark["x"],"y":landmark["y"]}
-        data_source["pose"] = result
-        data_source["timestamp"] = datetime.now()
-        csv_save.save_data(data_source)
+        # 画像をリードオンリーにしてposes検出処理実施
+        rgb_image.flags.writeable = False
+        pose_results = pose.process(rgb_image)
+        rgb_image.flags.writeable = True
 
-    # ディスプレイ表示
-    frame = draw_point_history(frame, point_history)
-    cv2.imshow('chapter03', frame)
+        # 有効なランドマークが検出された場合、ランドマークを描画
+        if pose_results.pose_landmarks:
+            mp_drawing.draw_landmarks(frame, pose_results.pose_landmarks,
+                                        mp_pose.POSE_CONNECTIONS)
 
-    # キー入力(ESC:プログラム終了)
-    key = cv2.waitKey(1)
-    if key == 27:  # ESC
-        break
+            # ランドマーク座標の計算
+            landmark_list = calc_landmark_list(rgb_image, pose_results.pose_landmarks)
+            # 人差指の指先座標を履歴に追加
+            # point_history.append(landmark_list[ID_FINGER_TIP])
 
+            result = {}
+            data_source = {}
+            #for i,landmark in enumerate(pose_results.pose_landmarks.landmark):
+            
+            # ランドマークの取得
+            for i,landmark in enumerate(landmark_list):
+                result[mp_pose.PoseLandmark(i).name] = {"x":landmark["x"],"y":landmark["y"]}
+            data_source["pose"] = result
+            data_source["timestamp"] = datetime.now()
+            # csvに書き込み
+            csv_save.save_data(data_source)
 
-    frame_count += 1
+        # ディスプレイ表示
+        frame = draw_point_history(frame, point_history)
+        cv2.imshow('preview', frame)
+
+        # キー入力(ESC:プログラム終了)
+        key = cv2.waitKey(1)
+        if key == 27:  # ESC
+            break
+
+        frame_count += 1
+
 
 # リソースの解放
 video_capture.release()
